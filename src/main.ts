d7e2b389dce3ea36777cb7fa3911dd268c103387
Sebastian Sandner <sebastian.witthus@googlemail.com>
2022-08-22 13:57:42 +0200

    Delete yarn.lock.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
+import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function bootstrap() 
+{
   const app = await NestFactory.create(AppModule);
   await app.init();
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT', 3000);
   await app.listen(port);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}`
+  );
 }
 bootstrap();
